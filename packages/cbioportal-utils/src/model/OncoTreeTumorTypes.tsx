84dfc7fa2447dfd657312e2822c19041ae7c49f6
AnhVu.Tran <anhvu.tran@uksh.de>
2026-03-06 11:09:43 +0100

    feat(module-packages): port MyCancerGenome data layer and OncoKB EMA drug enhancements

---
+++ packages/cbioportal-utils/src/model/OncoTreeTumorTypes.tsx
@@ -0,0 +1,47 @@
+type TumorType = {
+    code?: string;
+    color?: string;
+    name?: string;
+    mainType?: string;
+    externalReferences?: {
+        UMLS?: Array<string>;
+        NCI?: Array<string>;
+    };
+    tissue?: string;
+    children?: object;
+    parent?: string;
+    history?: Array<any>;
+    level?: number;
+    revocations?: Array<any>;
+    precursors?: Array<any>;
+};
+
+function loadOncoTreeTumorTypes(): TumorType[] {
+    if (
+        typeof window === 'undefined' ||
+        typeof XMLHttpRequest === 'undefined'
+    ) {
+        return [];
+    }
+
+    let url = '/tumorTypes.json';
+    if (
+        window.location.hostname === 'localhost' ||
+        window.location.port === '3000'
+    ) {
+        url = 'https://oncotree.info/api/tumorTypes';
+    }
+
+    try {
+        const getRequest = new XMLHttpRequest();
+        getRequest.open('GET', url, false);
+        getRequest.send(null);
+        return JSON.parse(getRequest.responseText);
+    } catch (error) {
+        return [];
+    }
+}
+
+const oncoTreeTumorTypes: TumorType[] = loadOncoTreeTumorTypes();
+
+export default oncoTreeTumorTypes;
